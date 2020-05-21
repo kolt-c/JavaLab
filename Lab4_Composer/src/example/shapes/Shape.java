efe3465e0a69eb35cee343fbc7c742473b6d128d
Kolt-c <ts.nicolas@outlook.com>
2020-05-21 20:27:11 +0300

    Lab4 Composer method

---
+++ Lab4_Composer/src/example/shapes/Shape.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example.shapes;
+
+/**
+ *
+ * @author 1cadmin
+ */
+
+import java.awt.*;
+
+public interface Shape {
+    int getX();
+    int getY();
+    int getWidth();
+    int getHeight();
+    void move(int x, int y);
+    boolean isInsideBounds(int x, int y);
+    void select();
+    void unSelect();
+    boolean isSelected();
+    void paint(Graphics graphics);
+}
